20946657c422a31c75716a7d28ab5db819920a18
Israel Kusayev <israelk@diagnosticrobotics.com>
2022-02-21 04:16:23 +0200

    home page

--- client/screens/HomeScreen.tsx
+++ client/screens/HomeScreen.tsx
@@ -1,14 +1,35 @@
-import { StyleSheet } from 'react-native';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useEffect, useState } from "react";
+import { StyleSheet } from "react-native";
 
-// import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
-import Cards from '../components/Cards';
-import TinderDishes from './TinderDishes';
+import { Text, View } from "../components/Themed";
+import { RootTabScreenProps } from "../types";
+import { CuisineTypeIds, PreferencesType } from "./FiltersScreen";
+import TinderDishes from "./TinderDishes";
 
-export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
+export default function HomeScreen({ navigation }: RootTabScreenProps<"Home">) {
+  useEffect(() => {
+    const getFilters = async () => {
+      let preferences: PreferencesType[] = [];
+      let cuisineTypes: CuisineTypeIds[] = [];
+      const preferencesStr = await AsyncStorage.getItem("preferences");
+      if (preferencesStr) {
+        preferences = JSON.parse(preferencesStr);
+      }
+      const cuisineTypesStr = await AsyncStorage.getItem("cuisineTypes");
+      if (cuisineTypesStr) {
+        cuisineTypes = JSON.parse(cuisineTypesStr);
+      }
+      if (preferences.length !== 0 && cuisineTypes.length !== 0) {
+        navigation.push("Tinder", { preferences, cuisineTypes });
+      }
+    };
+    getFilters();
+  }, []);
+
   return (
     <View style={styles.container}>
+      <Text>Please Select Filters</Text>
       {/* <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
@@ -19,16 +40,16 @@ export default function HomeScreen({ navigation }: RootTabScreenProps<'Home'>) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
