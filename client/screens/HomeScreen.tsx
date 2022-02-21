fe662946fa2ffbdfbe824155fc6602add55fde03
Matan Haliva <matanh@diagnosticrobotics.com>
2022-02-21 02:59:55 +0200

    remove cards components

--- client/screens/HomeScreen.tsx
+++ client/screens/HomeScreen.tsx
@@ -4,14 +4,14 @@ import { StyleSheet } from 'react-native';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 import Cards from '../components/Cards';
+import TinderDishes from './TinderDishes';
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+export default function HomeScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
       {/* <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" /> */}
-      <Cards />
     </View>
   );
 }
--- client/screens/TinderDishes.tsx
+++ client/screens/TinderDishes.tsx
@@ -1,12 +1,93 @@
 import { Text, View } from '../components/Themed';
 import { TinderProps } from '../types';
-import { useGetDishes } from '../hooks/useDishes';
 import { useEffect } from 'react';
+import React, { useState } from 'react'
+import styled from 'styled-components/native'
+import TinderCard from 'react-tinder-card'
+import { Avatar } from 'react-native-paper';
+import { useGetDishes } from '../hooks/useDishes'
+import { Switch } from 'react-native-switch';
 
+
+const Container = styled.View`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    justify-content: flex-start;
+`
+
+const Header = styled.Text`
+    color: #ffffff;
+    font-size: 30px;
+    margin-bottom: 30px;
+`
+
+const CardContainer = styled.View`
+    flex: 1 1 auto;
+    display: flex;
+    width: 90%;
+    max-width: 350px;
+    height: 300px;
+`
+
+const Card = styled.View`
+    position: absolute;
+    background-color: #fff;
+    width: 100%;
+    max-width: 350px;
+    height: 500px;
+    shadow-color: black;
+    shadow-opacity: 0.2;
+    shadow-radius: 20px;
+    border-radius: 20px;
+    resize-mode: cover;
+`
+
+const CardImage = styled.ImageBackground`
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    border-radius: 20px;
+`
+
+const CardTitle = styled.Text`
+    position: absolute;
+    bottom: 0;
+    margin: 10px;
+    color: black;
+`
+
+const InfoText = styled.Text`
+    margin-top: 2px;
+    height: 28px;
+    justify-content: center;
+    display: flex;
+    z-index: -100;
+    color: white;
+`
+const FlexContainer = styled.Text`
+display: flex;
+justify-content: space-between;
+width: 100%;
+max-width: 350px;
+`
 
 export default function TinderDishes({ route, navigation }: TinderProps) {
 	const { preferences, cuisineTypes } = route.params;
-	const { data, isLoading, error } = useGetDishes({ cuisineTypes, preferences });
+	const [lastDirection, setLastDirection] = useState()
+	const [isSwitchOn, setIsSwitchOn] = React.useState(false);
+	const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+	const { isLoading, error, data: dishes } = useGetDishes({ cuisineTypes, preferences });
+
+	const swiped = (direction: any) => {
+		setLastDirection(direction)
+	}
+
+	const outOfFrame = (name: string) => {
+		console.log(name + ' left the screen!')
+	}
 
 	useEffect(() => {
 		if (error) {
@@ -18,14 +99,47 @@ export default function TinderDishes({ route, navigation }: TinderProps) {
 		return <Text>Loading...</Text>;
 	}
 
-	if (data) {
+	if (dishes) {
 		return (
-			<View>
-				<Text>Dishes:</Text>
-				{data.map((item, index) =>
-					<Text key={index}>dish name: {item.dish.dishName}</Text>
-				)}
-			</View>
+			<Container>
+				<Header>Mana Mana</Header>
+				{dishes && <CardContainer>
+					{dishes.map((dishe) =>
+						<TinderCard key={dishe.name} onSwipe={(dir) => swiped(dir)} onCardLeftScreen={() => outOfFrame(dishe.name)}>
+							<Card>
+								<CardImage source={{ uri: dishe.image }}>
+									<CardTitle>{dishe.name}</CardTitle>
+								</CardImage>
+							</Card>
+						</TinderCard>
+					)}
+				</CardContainer>}
+				<FlexContainer>
+					<Avatar.Icon style={{
+						backgroundColor: "white", //works on IOS only
+					}} color={"black"} size={48} icon="close" onClick={() => swiped("left")} />
+					<Avatar.Icon style={{
+						backgroundColor: "white", //works on IOS only
+					}} color={"red"} size={48} icon="cards-heart" onClick={() => swiped("right")} />
+				</FlexContainer>
+
+				{lastDirection ? <InfoText>You swiped {lastDirection}</InfoText> : <InfoText />}
+
+				<View style={{ marginBottom: 30 }}>
+					<Switch
+						switchWidthMultiplier={5}
+						switchBorderRadius={90}
+						value={isSwitchOn}
+						onValueChange={onToggleSwitch}
+						disabled={false}
+						activeText={'Total 34$'}
+						inActiveText={'Total 34$'}
+						backgroundActive={'green'}
+						backgroundInactive={'gray'}
+						circleActiveColor={'#30a566'}
+						circleInActiveColor={'#000000'} />
+				</View>
+			</Container>
 		);
 	}
 
